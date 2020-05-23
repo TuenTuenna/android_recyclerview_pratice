8c8d2c7143a0decd8f1c2b4b643f0d7c6fe2db30
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2020-05-23 13:39:49 +0900

    커스텀 어답터 & 클릭리스너

--- app/src/main/java/com/jeongdaeri/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/jeongdaeri/myrecyclerview/MainActivity.kt
@@ -3,13 +3,20 @@ package com.jeongdaeri.myrecyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.FieldPosition
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MyRecyclerviewInterface {
 
     val TAG: String = "로그"
 
     // 데이터를 담을 그릇 즉 배열
     var modelList = ArrayList<MyModel>()
+
+    private lateinit var myRecyclerAdapter: MyRecyclerAdapter
 
     // 뷰가 화면에 그려질때
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +34,43 @@ class MainActivity : AppCompatActivity() {
         }
 
         Log.d(TAG, "MainActivity - 반복문 돌린 후 this.modelList.size : ${this.modelList.size}")
+
+        // 어답터 인스턴스 생성
+        myRecyclerAdapter = MyRecyclerAdapter(this)
+
+        myRecyclerAdapter.submitList(this.modelList)
+
+        // 리사이클러뷰 설정
+        my_recycler_view.apply {
+
+            // 리사이클러뷰 방향 등 설정
+            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+
+            // 어답터 장착
+            adapter = myRecyclerAdapter
+        }
+
+    }
+
+    override fun onItemClicked(position: Int) {
+        Log.d(TAG, "MainActivity - onItemClicked() called / position: $position")
+
+        var name: String? = null
+
+        // 값이 비어있으면 ""를 넣는다.
+        // unwrapping - 언랩핑
+
+        val title: String = this.modelList[position].name ?: ""
+
+//        val title: String = name ?: "호호호"
+
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage("$title 와 함께하는 빡코딩! :)")
+            .setPositiveButton("오케이") { dialog, id ->
+                Log.d(TAG, "MainActivity - 다이얼로그 확인 버튼 클릭했음")
+            }
+            .show()
 
     }
 
--- app/src/main/java/com/jeongdaeri/myrecyclerview/MyViewHolder.kt
+++ app/src/main/java/com/jeongdaeri/myrecyclerview/MyViewHolder.kt
@@ -6,16 +6,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.layout_recycler_item.view.*
 
-class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+// 커스텀 뷰홀더
+class MyViewHolder(itemView: View,
+                   recyclerviewInterface: MyRecyclerviewInterface):
+                    RecyclerView.ViewHolder(itemView),
+                    View.OnClickListener
+{
 
     val TAG: String = "로그"
 
     private val usernameTextView = itemView.user_name_txt
     private val profileImageView = itemView.profile_img
 
+    private var myRecyclerviewInterface : MyRecyclerviewInterface? = null
+
     // 기본 생성자
     init {
         Log.d(TAG, "MyViewHolder - init() called")
+
+        itemView.setOnClickListener(this)
+        this.myRecyclerviewInterface = recyclerviewInterface
+
     }
 
 
@@ -33,6 +44,13 @@ class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 //            .centerCrop()
             .placeholder(R.mipmap.ic_launcher)
             .into(profileImageView)
+
+    }
+
+    override fun onClick(p0: View?) {
+        Log.d(TAG, "MyViewHolder - onClick() called")
+
+        this.myRecyclerviewInterface?.onItemClicked(adapterPosition)
 
     }
 
